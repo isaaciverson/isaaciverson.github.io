db0c3421a92a215f6fc89b95cb795e92005defe8
Isaac Iverson <isaaciverson1024@gmail.com>
2019-05-23 12:27:38 -0400

    Adds portfolio pages, js for links / Removes Toastify, JQuery

--- js/index.js
+++ js/index.js
@@ -1,22 +1,3 @@
-function showMobileMenu() {
-    var mobileMenu = document.getElementById("mobile-menu");
-    var mobileMenuBtn = document.getElementById("mobile-menu-btn-icon");
-    if (mobileMenu.classList.contains("show")) {
-        mobileMenu.classList.remove("show");
-    } else {
-        mobileMenu.classList.add("show");
-    }
-    if (mobileMenuBtn.classList.contains("fa-bars")) {
-        mobileMenuBtn.classList.remove("fa-bars");
-        mobileMenuBtn.classList.add("fa-times");
-    } else {
-        mobileMenuBtn.classList.remove("fa-times");
-        mobileMenuBtn.classList.add("fa-bars");
-    }
-}
-
-// MENU MOUSECLICK
-
 // MENU MOUSEOVERS
 
 function designMouseOver() {
